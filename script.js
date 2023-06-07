ec2674564eb3ff237f4a0c6fbf6d3c614e82f554
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-06-07 16:18:21 +0200

    Base structure done

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.png',
+                    avatar: './img/avatar_6.jpg',
                     visible: true,
                     messages: [
                         {
